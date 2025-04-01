ddcff14cc59bee842d67f571994d6067b1dfa253
huybq <huybq@gmail.com>
2025-04-01 14:43:08 +0700

    fix error token

--- src/main/java/com/huybq/fund_management/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/huybq/fund_management/config/jwt/JwtAuthenticationFilter.java
@@ -30,22 +30,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-
-        // Nếu không có header Authorization hoặc không phải token dạng Bearer
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing or invalid token");
+        if (authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
             return;
         }
-
-        // Lấy token từ header Authorization
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
-
-        // Kiểm tra xem token có hợp lệ và người dùng có xác thực chưa
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (userEmail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            // Kiểm tra token có hợp lệ không
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            if (jwtService.isTokenValid(jwt,userDetails)){
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
@@ -53,17 +46,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 );
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else {
-                // Nếu token không hợp lệ hoặc hết hạn
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or expired token");
-                return;
             }
-        } else {
-            // Nếu không tìm thấy userEmail từ token
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access");
-            return;
         }
-
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
     }
 }
